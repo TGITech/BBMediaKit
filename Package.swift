225e7b5a6a5360217e3270fb5841cc0145160de0
Kovtun Vladimir <liamzik89@gmail.com>
2024-09-02 08:27:41 +0300

    added library

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "BBMediaAugmentationKit",
-            targets: ["BBMediaKit"]
+            targets: ["BBMediaKit", "BBMediaKitSDK"]
         ),
     ],
     dependencies: [
